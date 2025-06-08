fd87cf5381359ccd39341539046b03135477426c
tpdkrt <tpodko@gmail.com>
2025-06-08 13:11:13 +0200

    remade the navbar based on a tutrorial i liked but further modifiactions will ensue

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,4 +1,4 @@
-function putNavbar(){
+/*function putNavbar(){
     let navbarEl = document.querySelector(".navbar");
     if (navbarEl){
         navbarEl.innerHTML = `
@@ -17,6 +17,35 @@ function putNavbar(){
             <div>about me</div>
         </a>
         </div>
+        `;
+    } else {
+        console.log("navbar not found on page");
+    }
+}*/
+
+function putNavbar(){
+    let navbarEl = document.querySelector(".navbar");
+    if (navbarEl){
+        navbarEl.innerHTML = `
+        <button id="open-sidebar-button" aria-label="open sidebar" aria-expanded="false" aria-controls="navbar">
+        <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#c9c9c9"><path d="M165.13-254.62q-10.68 0-17.9-7.26-7.23-7.26-7.23-18t7.23-17.86q7.22-7.13 17.9-7.13h629.74q10.68 0 17.9 7.26 7.23 7.26 7.23 18t-7.23 17.87q-7.22 7.12-17.9 7.12H165.13Zm0-200.25q-10.68 0-17.9-7.27-7.23-7.26-7.23-17.99 0-10.74 7.23-17.87 7.22-7.13 17.9-7.13h629.74q10.68 0 17.9 7.27 7.23 7.26 7.23 17.99 0 10.74-7.23 17.87-7.22 7.13-17.9 7.13H165.13Zm0-200.26q-10.68 0-17.9-7.26-7.23-7.26-7.23-18t7.23-17.87q7.22-7.12 17.9-7.12h629.74q10.68 0 17.9 7.26 7.23 7.26 7.23 18t-7.23 17.86q-7.22 7.13-17.9 7.13H165.13Z"/></svg>
+        </button>
+
+        <nav id="navbar">
+            <ul>
+            <li>
+                <button id="close-sidebar-button" aria-label="close sidebar">
+                <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#c9c9c9"><path d="m480-444.62-209.69 209.7q-7.23 7.23-17.5 7.42-10.27.19-17.89-7.42-7.61-7.62-7.61-17.7 0-10.07 7.61-17.69L444.62-480l-209.7-209.69q-7.23-7.23-7.42-17.5-.19-10.27 7.42-17.89 7.62-7.61 17.7-7.61 10.07 0 17.69 7.61L480-515.38l209.69-209.7q7.23-7.23 17.5-7.42 10.27-.19 17.89 7.42 7.61 7.62 7.61 17.7 0 10.07-7.61 17.69L515.38-480l209.7 209.69q7.23 7.23 7.42 17.5.19 10.27-7.42 17.89-7.62 7.61-17.7 7.61-10.07 0-17.69-7.61L480-444.62Z"/></svg>
+                </button>
+            </li>
+            <li class="home-li"><a class="active-link" aria-current="page" href="index.html">Home</a></li>
+            <li><a href="index.html#main_title">About</a></li>
+            <li><a href="index.html#edu_title">Education</a></li>
+            <li><a href="projects.html">Projects</a></li>
+            </ul>
+        </nav>
+
+        <div id="overlay" onclick="closeSidebar()" aria-hidden="true"></div>
         `;
     } else {
         console.log("navbar not found on page");
@@ -52,6 +81,57 @@ async function isR(){
     }
 }
 
+function updateNavbar(e){
+    const isMobile = e.matches
+    console.log(isMobile)
+    if(isMobile){
+      navbar.setAttribute('inert', '')
+    }
+    else{
+      // desktop device
+      navbar.removeAttribute('inert')
+    }
+  }
+  
+function openSidebar(){
+    console.log("opening sidebar");
+    navbar.classList.add('show')
+    openButton.setAttribute('aria-expanded', 'true')
+    navbar.removeAttribute('inert')
+}
+  
+function closeSidebar(){
+    console.log("closing sidebar");
+    navbar.classList.remove('show')
+    openButton.setAttribute('aria-expanded', 'false')
+    navbar.setAttribute('inert', '')
+}
+  
+
 isR();
 
 putNavbar();
+
+const openButton = document.getElementById('open-sidebar-button');
+const navbar = document.getElementById('navbar');
+const closeButton = document.getElementById('close-sidebar-button');
+
+//We check the presence of elements
+if(!openButton){
+    console.log("No open button found");
+}
+if(!navbar){
+    console.log("No navbar found");
+}
+if(!closeButton){
+    console.log('No close navbar button found');
+}
+
+const media = window.matchMedia("(width < 700px)");
+
+media.addEventListener('change', (e) => updateNavbar(e));
+closeButton.addEventListener('click',()=>{closeSidebar();});
+openButton.addEventListener('click',()=>{openSidebar();});
+
+
+updateNavbar(media);
